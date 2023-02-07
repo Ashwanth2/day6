b794600cc08720b21aba5de12de38e0243abe619
Ashwanth2 <kaviashwanth344@gmail.com>
2023-02-07 21:17:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//1.class movie
+// a)
+// public Movie("t","s","r") {
+//     title ="t";
+//     studio = "s";
+//      rating = "r";
+// }
+
+// b)
+// public Movie ("t","s") {
+//     title ="t";
+//     studio ="s";
+//     rating = "PG";
+// }
+// c)
+// public static Movie[] getPG(Movie[] mov) {
+//     Movie[] pgMov = new Movie[mov.length];
+//     int newArrayIndex = 0;
+//     for (let i=0; i<mov.length; i++) {
+//     if (mov[i].rating = ("PG")) {
+//     pgMov[newArrayIndex] = mov[i];
+//     newArrayIndex++;
+//     }
+//     }
+//     return pgMov;
+// }
+// d)
+// Movie mov = new Movie("Casino Royale","Eon Productions","PG-13");
+
+//3)
